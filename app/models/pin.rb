7c94916c2a0be8a647d3ebb91d18eeae5547c2f2
Valerie Carrasco <valerie.carrasco25@gmail.com>
2015-11-29 19:16:12 -0500

    Trying to have same Ruby version in Heroku

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,9 +4,12 @@ class Pin < ActiveRecord::Base
 
   belongs_to :category
 
- attr_accessor :category_id, :image_content_type, :image_file_name
 
- has_attached_file :image, styles: { medium: "300x300>", thumb: "75x75>" }, default_url: "http://placebear.com/75/75"
+  attr_accessor :category_id, :image_content_type, :image_file_name, :category, :image
 
- validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "75x75>" }, default_url: "http://placebear.com/75/75"
+
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+
 end
